8978bc25ab0e44257d26bcb70bc45364ccbab785
arisgi <m1so.tama16@gmail.com>
2017-06-15 19:35:52 +0900

    Add style sheets

--- fuse.js
+++ fuse.js
@@ -1,11 +1,17 @@
-const { FuseBox } = require('fuse-box');
+const { FuseBox, CSSPlugin, SassPlugin, WebIndexPlugin } = require('fuse-box');
 
 const fuse = FuseBox.init({
   homeDir: 'src',
-  output: 'dist/$name.js'
+  output: 'dist/$name.js',
+  plugins: [
+    [SassPlugin(), CSSPlugin()],
+    CSSPlugin(),
+    WebIndexPlugin({ path: '.' })
+  ]
 });
 
 fuse.bundle('app')
+  .watch()
   .instructions('> index.ts');
 
 fuse.run();
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+/*
 import { hello } from './hello';
 import * as moment from 'moment';
 
@@ -5,3 +6,12 @@ const time = moment().format('MMMM Do YYYY, h:mm:ss a');
 const name: string = 'FuseBox';
 console.log(hello(name));
 console.log(time);
+*/
+
+import './main.css';
+import './extra.scss';
+
+document.body.innerHTML = `
+<div class="content">
+  <h1>Welcome to FuseBox</h1>
+</div>`;
